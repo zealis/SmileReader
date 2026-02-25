3dce15e0c820890f8aef91aaa0ebda141dee19da
zealis <zealisnex@outlook.com>
2026-02-25 15:37:00 +0800

    Update Android build files

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,5 +1,11 @@
 allprojects {
     repositories {
+        maven {
+            url = uri("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/")
+        }
+        maven {
+            url = uri("https://mirrors.cloud.tencent.com/nexus/repository/google/")
+        }
         google()
         mavenCentral()
     }
